ce9ad40fc29c0836111864ff4871f6451fdf617e
piyanka <priyankakumari_ee21b9_01@dtu.ac.in>
2024-12-24 01:16:58 +0530

    edit index.tsx

--- components/FeedCard/index.tsx
+++ components/FeedCard/index.tsx
@@ -1,7 +1,15 @@
 import React from "react"
 
 const FeedCard : React.FC = () => {
-    return <div></div>
+    return 
+    <div>
+        <div className=" border grid gird-cols-12">
+            <div className="col-span-2 "></div>
+            <div className="col-span-10 "></div>
+
+        </div>
+
+    </div>
 }
 
 export default FeedCard;
